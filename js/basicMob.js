39ad71cce4a6f9128faf14a71edb0924e01056a9
eric.tumon <eric.tumon@gmail.com>
2023-08-13 20:11:14 +0800

    *successfully made a basic mob child from the mob parent. *basic mob dies once its radius reaches 5 *changed a few entities color

--- js/basicMob.js
+++ js/basicMob.js
@@ -1,7 +1,7 @@
 class BasicMob extends Mob {
 	constructor(x, y, radius, color, damage) {
-		super(x, y, radius, color, damage);
-		this.health = radius;
+		super(x, y, radius, color, radius, damage);
+//		this.health = radius;
 		this.startingPos = {
 			x, y
 		}
